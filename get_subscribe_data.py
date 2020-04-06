a26e69b5983ebb054057b25d38a530288b02d3ef
zhaoshengzhi <ricky2743@163.com>
2020-04-06 22:13:27 +0800

    ping

--- get_subscribe_data.py
+++ get_subscribe_data.py
@@ -1,6 +1,5 @@
-from urllib.request import ProxyHandler, build_opener
-from urllib.request import Request
 from urllib.request import urlopen
+from urllib.request import Request
 from urllib.error import URLError
 from base64 import b64decode
 import subprocess
@@ -32,7 +31,6 @@ class SubscribeHandler:
     def _load_vm_list(self):
         node_list = []
         if not os.path.exists(self._vm_list):
-            print('connecting...')
             # load by network
             for u in self.urls:
                 node_list.extend(self._update_vm_list(u))
@@ -48,6 +46,7 @@ class SubscribeHandler:
 
     # get nodes from remote server
     def _update_vm_list(self, subscribe_url):
+        print('connecting {}'.format(subscribe_url))
         res = []
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
         req = Request(url=subscribe_url, headers=headers)
@@ -55,8 +54,6 @@ class SubscribeHandler:
         # get vmess://......
         links = b64decode(content + '==').decode('utf-8').splitlines()
         # save nodes to file
-        if os.path.exists(self._vm_list):
-            os.remove(self._vm_list)
         with open(self._vm_list, 'a+', encoding='utf-8') as f_vm_list:
             for link in links:
                 # decode vmess protocol to json string
@@ -66,21 +63,22 @@ class SubscribeHandler:
         return res
 
     # test node status
-    def _test_ping(self):
+    def _test_ping(self, node):
+        self._write_to_config(node)
         socks.set_default_proxy(socks.SOCKS5, '127.0.0.1', 1080)
         socket.socket = socks.socksocket
         try:
             time_start = time.time()
             response = urlopen('https://www.google.com')
             # print(response.read().decode('utf-8'))
-            time_end = time.time() # todo...
-            return 1
+            time_end = time.time()  # todo...
+            return time_end - time_start
         except URLError as e:
             # print(e.reason)
             return -1
 
     # update config.json
-    def _write_to_config(self, vm):
+    def _write_to_config(self, target_node):
         with open(os.path.join(self._root_path, 'config.json.template'), 'r', encoding='utf-8') as f_template:
             config_content = f_template.read()
         # $xx is placeholder
@@ -88,13 +86,13 @@ class SubscribeHandler:
         error_log = os.path.join(self._v2ray_path, 'log', 'error.log')
         config_content = config_content.replace('$ACCESS_LOG', access_log)
         config_content = config_content.replace('$ERROR_LOG', error_log)
-        config_content = config_content.replace('$ADDRESS', vm['add'])
-        config_content = config_content.replace('$HOST', vm['host'])
-        config_content = config_content.replace('$USER_ID', vm['id'])
-        config_content = config_content.replace('$NET', '"{}"'.format(vm['net']) if vm['net'] != '' else 'null')
-        config_content = config_content.replace('$PATH', vm['path'])
-        config_content = config_content.replace('$PORT', vm['port'])
-        config_content = config_content.replace('$TLS', '"{}"'.format(vm['tls']) if vm['tls'] != '' else 'null')
+        config_content = config_content.replace('$ADDRESS', target_node['add'])
+        config_content = config_content.replace('$HOST', target_node['host'])
+        config_content = config_content.replace('$USER_ID', target_node['id'])
+        config_content = config_content.replace('$NET', '"{}"'.format(target_node['net']) if target_node['net'] != '' else 'null')
+        config_content = config_content.replace('$PATH', target_node['path'])
+        config_content = config_content.replace('$PORT', target_node['port'])
+        config_content = config_content.replace('$TLS', '"{}"'.format(target_node['tls']) if target_node['tls'] != '' else 'null')
         config_json = os.path.join(self._v2ray_path, 'config.json')
         if os.path.exists(config_json):
             os.remove(config_json)
@@ -117,21 +115,48 @@ class SubscribeHandler:
         self._terminate_v2ray()
         self._run_v2ray()
 
+    @staticmethod
+    def _print_functions():
+        print('===========menu==========\n')
+        print('1: update node list\n')
+        print('2: ping node list\n')
+        print('3: use the fastest node\n')
+
     def start(self):
         nodes = self._load_vm_list()
+        nodes_delay = {}
         if not self._run_v2ray():
             print('run v2ray failed')
             return
         print('testing...')
+        time.sleep(1)  # wait process run
         sec = self._test_ping()
         print('current delay is {}ms'.format(sec))
         print('*******************************')
         while True:
-            op = input('enter q to quit\n')
+            op = input('enter q to quit, h to show functions\n')
             if op == 'q':
                 print('Bye')
                 self._terminate_v2ray()
                 return
+            elif op == 'h':
+                self._print_functions()
+            elif op == '1':
+                print('updating...')
+                if os.path.exists(self._vm_list):
+                    os.remove(self._vm_list)
+                nodes = self._load_vm_list()
+            elif op == '2':
+                for node in nodes:
+                    print('testing {}'.format(node['add']))
+                    r = self._test_ping(node)
+                    nodes_delay[node['add']] = r
+                    print('use {} ms'.format(str(r)))
+                print('done!\n')
+            elif op == '3':
+                pass
+            else:
+                print('input invalid!\n')
 
 
 if __name__ == '__main__':
